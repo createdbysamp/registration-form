7168da3cbebcb3f43de85b06cf999b61489f4f57
Sampeezy <createdbysamp@gmail.com>
2025-08-07 16:09:37 -0500

    phase 2

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 function RegistrationForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [funFact, setFunFact] = useState("");
+  const [favoriteLanguage, setFavoriteLanguage] = useState("None");
+  const [agreedToTerms, setAgreedToTerms] = useState(false);
 
   const handleUsernameChange = (e) => setUsername(e.target.value);
   const handleEmailChange = (e) => setEmail(e.target.value);
@@ -10,7 +13,11 @@ function RegistrationForm() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    alert(`Submitted Username: ${username} \nSubmitted Email: ${email}`);
+    alert(
+      `Submitted Username: ${username} \nSubmitted Email: ${email} \nCreated Fun Fact: ${funFact} \nSelected Fav Language: ${favoriteLanguage} \nAgreed to Terms: ${
+        agreedToTerms ? "Yes" : "No"
+      }`
+    );
 
     setUsername("");
     setEmail("");
@@ -18,21 +25,60 @@ function RegistrationForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
+      <div id="username">
         <label>
           Username:
           <input type="text" value={username} onChange={handleUsernameChange} />
         </label>
       </div>
-      <div>
+      <div id="email">
         <label>
           Email:
           <input type="email" value={email} onChange={handleEmailChange} />
         </label>
       </div>
+      <div id="fun-fact">
+        <label>
+          Fun Fact About You:
+          <textarea
+            value={funFact}
+            onChange={(e) => setFunFact(e.target.value)}
+          />
+        </label>
+      </div>
+      <div id="favorite-language">
+        <label>
+          Favorite Language:
+          <select
+            value={favoriteLanguage}
+            onChange={(e) => setFavoriteLanguage(e.target.value)}
+          >
+            <option value="None">Please select a language</option>
+            <option value="HTML">HTML</option>
+            <option value="CSS">CSS</option>
+            <option value="SQL">SQL</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="C#">C#</option>
+            <option value="Python">Python</option>
+          </select>
+        </label>
+      </div>
+      <div id="agreed-to-terms">
+        <label>
+          <input
+            type="checkbox"
+            checked={agreedToTerms}
+            onChange={(e) => setAgreedToTerms(e.target.checked)}
+          />
+          I agree to the terms and conditions
+        </label>
+      </div>
       <button type="submit">Register</button>
       <p>Current Username: {username}</p>
       <p>Current Email: {email}</p>
+      <p>Current Fun Fact: {funFact}</p>
+      <p>Current Favorite Language: {favoriteLanguage}</p>
+      <p>Agreed to Terms: {agreedToTerms ? "Yes" : "No"}</p>
     </form>
   );
 }
--- src/components/RegistrationForm2.jsx
+++ src/components/RegistrationForm2.jsx
@@ -4,32 +4,42 @@ function RegistrationForm2() {
   const [formData, setFormData] = useState({
     username: "",
     email: "",
+    funFact: "",
+    favoriteLanguage: "None",
+    agreedToTerms: false,
   });
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    const { name, value, checked } = event.target;
 
     setFormData((prevFormData) => ({
       ...prevFormData,
-      [name]: value,
+      [name]: event.target.type === "checkbox" ? checked : value,
     }));
   };
   const handleSubmit = (event) => {
     event.preventDefault();
 
     alert(
-      `Submitted Username: ${formData.username} \nSubmitted Email: ${formData.email}`
+      `Submitted Username: ${formData.username} \nSubmitted Email: ${
+        formData.email
+      } \nCreated Fun Fact: ${formData.funFact} \nSelected Fav Language: ${
+        formData.favoriteLanguage
+      } \nAgreed to Terms: ${formData.agreedToTerms ? "Yes" : "No"}`
     );
 
     setFormData({
       username: "",
       email: "",
+      funFact: "",
+      favoriteLanguage: "(None)",
+      agreedToTerms: false,
     });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="mb-3">
+      <div id="username" className="mb-3">
         <label>
           Username:
           <input
@@ -40,7 +50,7 @@ function RegistrationForm2() {
           />
         </label>
       </div>
-      <div className="mb-2">
+      <div id="email" className="mb-2">
         <label>
           Email:
           <input
@@ -51,11 +61,53 @@ function RegistrationForm2() {
           />
         </label>
       </div>
+      <div id="fun-fact">
+        <label>
+          Fun Fact About You:
+          <textarea
+            value={formData.funFact}
+            name="funFact"
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div id="favorite-language">
+        <label>
+          Favorite Language:
+          <select
+            value={formData.favoriteLanguage}
+            name="favoriteLanguage"
+            onChange={handleChange}
+          >
+            <option value="None">Please select a language</option>
+            <option value="HTML">HTML</option>
+            <option value="CSS">CSS</option>
+            <option value="SQL">SQL</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="C#">C#</option>
+            <option value="Python">Python</option>
+          </select>
+        </label>
+      </div>
+      <div id="agreed-to-terms">
+        <label>
+          <input
+            type="checkbox"
+            name="agreedToTerms"
+            checked={formData.agreedToTerms}
+            onChange={handleChange}
+          />
+          I agree to the terms and conditions
+        </label>
+      </div>
       <button className="mb-2" type="submit">
         Register
       </button>
       <p>Current Username: {formData.username}</p>
       <p>Current Email: {formData.email}</p>
+      <p>Current Fun Fact: {formData.funFact}</p>
+      <p>Current Favorite Language: {formData.favoriteLanguage}</p>
+      <p>Agreed to Terms: {formData.agreedToTerms ? "Yes" : "No"}</p>
     </form>
   );
 }
